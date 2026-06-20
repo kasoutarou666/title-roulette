aa3671d79b4fb6ccb2fccb8bf6f1eb7756aa2d82
kasoutarou666 <akihiro4689@gmail.com>
2026-06-20 11:26:10 +0900

    feat: add Farcaster frame SDK ready call

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { WagmiProvider } from "wagmi";
+import sdk from "@farcaster/frame-sdk";
 
 import App from "./App.tsx";
 import { config } from "./wagmi.ts";
@@ -10,12 +11,22 @@ import "./index.css";
 
 const queryClient = new QueryClient();
 
+function Root() {
+  useEffect(() => {
+    sdk.actions.ready();
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </React.StrictMode>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </WagmiProvider>
-  </React.StrictMode>,
+  <Root />
 );
